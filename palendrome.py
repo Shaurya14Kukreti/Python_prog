06e6b4f7f7ace9bacd2c33b173de5974c4b71f6f
Shaurya Kukreti <37680988+Shaurya14Kukreti@users.noreply.github.com>
2018-04-09 08:52:32 +0000

    Update palendrome.py

--- palendrome.py
+++ palendrome.py
@@ -1,4 +1,4 @@
-
+# function to reverse the string
 def reverse(s):
   str = ""
   for i in s:
@@ -7,29 +7,31 @@ def reverse(s):
   return str    
 
 
-
+#starting of program take A man, a plan, a canal: Panama as input 
 string = "A man, a plan, a canal: Panama" 
+y=''    # empty string
 
-st = string.replace(" ", "")
-t =st.casefold()
-
-y=''
-for x in t:
-  if(((x >="a")and(x<="z"))or('0'<=x <='9')):
+#check for string if it contains characters other than the defined below range ,if yes skip the character  
+for x in string:
+  if(((x >="a")and(x<="z"))or('0'<=x <='9')or(x>="A")and (x<="Z")):
       y +=x
       
   else:
      continue
- 
-#print("String after removal of special characters is  ::",y)
+      
+ # After checking the string  becames (without " " and special characters ): 
+print("String after removal of special characters is  ::",y)
 
+# check if string becames empty or is empty after for loop  
 if (y==""):
    print("string is empty")
-   
+    
+#call reverse function    
 str1 = reverse(y)
-#print( "String after reverse function is  ::  ",str1)
+print( "String after reverse function is  ::  ",str1)
 
-if(y==str1):
+#put condition to check whether both strings y and str are equal or not:
+if(y.casefold() ==str1.casefold()):
   print("String is palendrome  ")
 else:
   print("String s not palendrome")
