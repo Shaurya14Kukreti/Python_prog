d37c73110075e421ef92f7785f19e0066322cefd
Shaurya Kukreti <37680988+Shaurya14Kukreti@users.noreply.github.com>
2018-04-09 09:13:38 +0000

    Update subarray.py

--- subarray.py
+++ subarray.py
@@ -1,3 +1,5 @@
+#function subarray : it will create all possible continuous subarrays and return it to str .
+#use print to see possible combinations
 def subarray(li1):
     sublist = []
     
@@ -9,16 +11,18 @@ def subarray(li1):
              
           
     return sublist
- 
+#Starting of program ,take a sample list
 l1 =[2,1,-3,4,-1,2,1,-5,4]
 f=0
+#call subarray() with l1 as argument 
 str =subarray(l1)
 #print(str)
 
+# iterating each list in str to calculate sum
 for i in range(len(str)):
     s =sum(str[i])
  
-   
+#  compare and print each sum till we found the greatest sun and its corresponding subarray   
     if s>=f:
       f=s
       print("the subarray {} has max sum ::{}".format(str[i],s))
